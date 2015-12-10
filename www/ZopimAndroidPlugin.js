107942ff82ad0fe97315aa0f8c5e99aeea04bb67
mbedoya <mauricio.bedoya@gmail.com>
2015-12-10 15:50:02 -0500

    echo function added

--- www/ZopimAndroidPlugin.js
+++ www/ZopimAndroidPlugin.js
@@ -1,6 +1,22 @@
 var ZopimAndroid = function() {
 };
 
+//echo
+ZopimAndroid.prototype.echo = function(successCallback, errorCallback, options) {
+    if (errorCallback == null) { errorCallback = function() {}}
+
+    if (typeof errorCallback != "function")  {
+        console.log("PushNotification.register failure: failure parameter not a function");
+        return
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("PushNotification.register failure: success callback parameter must be a function");
+        return
+    }
+
+    cordova.exec(successCallback, errorCallback, "ZopimAndroid", "echo", [options]);
+};
 
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
 ZopimAndroid.prototype.init = function(successCallback, errorCallback, options) {
